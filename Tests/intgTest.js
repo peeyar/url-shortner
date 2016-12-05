f835f5847e5751789fd5b73d846806c3658e153f
Rajesh Ramachandra <rajesh@Rajeshs-MacBook-Pro.local>
2016-12-04 23:08:16 -0800

    Initial commit

--- Tests/intgTest.js
+++ Tests/intgTest.js
@@ -6,28 +6,6 @@ const expect = require('chai').expect;
 
 const server = supertest.agent("http://localhost:5000");
 
-<<<<<<< HEAD
-// Here is the integration tests...
-
-
-describe("Basic test" , function(){
-	it('should return an object with key values' , function(done){
-		server.get('/X')
-			.set("Accept" ,'application/html')
-			.expect(200)
-			.end(function(err,res){
-			   expect(res.body).to.have.property("url");
-			   expect(res.body.url).to.not.equal(null);
-			   expect(res.body).to.have.property("shortUrl");
-			   expect(res.body).to.not.equal(null);
-			   expect(res.body).to.have.property("_id");
-			   done();
-			});
-	});
-});
-
-=======
->>>>>>> 371ec7b821ec07e213d197dda645be65fe9e7653
 describe("Home page  test",function(){
 	it("should return home page",function(done){
     // calling home page api...
@@ -70,4 +48,42 @@ describe("302 unit test",function(){
 	    });
 	  })
 	});
+
+
+
+describe('/POST url', () => {
+    it('it should not POST a wrong formatted url', (done) => {
+      let url = {
+          url: "The Lord of the Rings"
+      }
+      chai.request(server)
+          .post('/api/shorten')
+          .send(url)
+          .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a('object');
+              res.body.should.have.property('errors');
+              res.body.errors.should.have.property('pages');
+              res.body.errors.pages.should.have.property('kind').eql('required');
+            done();
+          });
+    });
+    it('it should POST a url ', (done) => {
+      let url = {
+          long_url: "https://hub.docker.com/r/peeyar/urlshorter/tags/"
+      }
+      chai.request(server)
+          .post('/api/shorten')
+          .send(book)
+          .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a('object');
+              res.body.book.should.have.property('_id');
+              res.body.book.should.have.property('createdOn');
+              res.body.book.should.have.property('long_url');
+              
+             done();
+          });
+    });
+});
 

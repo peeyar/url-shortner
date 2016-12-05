6e094ee4f54a9034b0b1c1e91267688b52f4ce5f
Rajesh Ramachandra <rajesh@Rajeshs-MacBook-Pro.local>
2016-12-05 00:40:35 -0800

    changed hostfile

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 let config = {};
 
 config.db = {};
-//config.webhost ='https://raj-url-shortner.herokuapp.com/';
-config.webhost ='https://localhost/';
+config.webhost ='https://raj-url-shortner.herokuapp.com/';
+//config.webhost ='https://localhost/';
 config.db.host = 'raj:raj123@ds111748.mlab.com:11748';
 config.db.name = 'urlshort';
 
